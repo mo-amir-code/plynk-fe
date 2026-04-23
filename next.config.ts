1defbe0186b6582965f344ce71846f51077208f6
Shubham071122 <shubhamkumar7112002@gmail.com>
2026-04-23 15:00:27 +0530

    feat: add Docker support with standalone output configuration and dynamic backend URL support

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: process.env.BACKEND_API_URL || "http://localhost:8080/api/:path*",
       },
     ];
   },
